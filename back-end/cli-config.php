b2f139448139f63a116b80a2ad8be29583fed6f6
Wesley Viana <wesley.vmartins.js@gmail.com>
2024-05-28 23:19:34 -0300

    Configurado doctrine migrations

---
+++ back-end/cli-config.php
@@ -0,0 +1,14 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Doctrine\Migrations\Configuration\EntityManager\ExistingEntityManager;
+use Doctrine\Migrations\DependencyFactory;
+use Doctrine\Migrations\Configuration\Migration\PhpFile;
+use Easy\Wallet\Helper\EntityManagerCreator;
+
+$config = new PhpFile(__DIR__ . '/migrations.php');
+
+$entityManager = EntityManagerCreator::createEntityManager();
+
+return DependencyFactory::fromEntityManager($config, new ExistingEntityManager($entityManager));
