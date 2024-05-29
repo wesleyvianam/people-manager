e9082f8ac5c2fff444ebb2bef40ca8aa324724e9
Wesley Viana <wesley.vmartins.js@gmail.com>
2024-05-28 23:22:26 -0300

    Criado model de Pessoa

---
+++ back-end/app/Domain/Entity/Person.php
@@ -0,0 +1,75 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Easy\Wallet\Domain\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping\GeneratedValue;
+use Doctrine\ORM\Mapping\Column;
+use Doctrine\ORM\Mapping\Entity;
+use Doctrine\ORM\Mapping\Id;
+use Doctrine\ORM\Mapping\OneToMany;
+
+#[Entity]
+class Person
+{
+    #[Id, Column, GeneratedValue]
+    private int $id;
+    #[Column]
+    private string $name;
+    #[Column]
+    private string $cpf;
+    #[OneToMany(
+        targetEntity: Contact::class,
+        mappedBy: "person",
+        cascade: ["persist", "remove"]
+    )]
+    private Collection $contacts;
+
+
+    public function __construct()
+    {
+        $this->contacts = new ArrayCollection();
+    }
+
+    public function getId(): int
+    {
+        return $this->id;
+    }
+
+    public function getName(): string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): void
+    {
+        $this->name = $name;
+    }
+
+    public function getCpf(): string
+    {
+        return $this->cpf;
+    }
+
+    public function setCpf(string $cpf): void
+    {
+        $this->cpf = $cpf;
+    }
+
+    public function addContact(Contact $contact): void
+    {
+        $this->contacts->add($contact);
+        $contact->setPerson($this);
+    }
+
+    /**
+     * @return Collection<Contact>
+     */
+    public function contacts(): Collection
+    {
+        return $this->contacts;
+    }
+}
