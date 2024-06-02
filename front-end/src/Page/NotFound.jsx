3ac2e89042aa8ce69527ce84a508a16a45a6b0d4
Wesley Viana <wesley.vmartins.js@gmail.com>
2024-06-02 18:32:05 -0300

    Adicionado página de Notfound

---
+++ front-end/src/Page/NotFound.jsx
@@ -0,0 +1,19 @@
+import {Link} from "react-router-dom";
+
+const NotFound = () => {
+    return (
+        <section className="bg-white">
+            <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+                <div className="mx-auto max-w-screen-sm text-center">
+                    <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-[#216F92]">404</h1>
+                    <p className="mb-4 text-3xl tracking-tight font-bold text-gray-900 md:text-4xl">Algo está faltando.</p>
+                    <p className="mb-4 text-lg font-light text-gray-500">Desculpe, não conseguimos encontrar essa página. Você encontrará muito para explorar na página inicial.</p>
+                    <Link to="/"
+                       className="inline-flex text-white bg-blue-600 hover:bg-primary-800 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center my-4">Página inicial</Link>
+                </div>
+            </div>
+        </section>
+    );
+}
+
+export default NotFound;
